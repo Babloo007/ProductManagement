0b43c3b4103ca8436eef8f0a080c974fdccfff76
Babloo007 <msvreddy77@gmail.com>
2023-05-25 23:59:12 +0530

    final commits

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,27 +9,26 @@ public class Main {
 
         ProductService service = new ProductService();
 
-        service.addProduct(new Product("Type C","Cable" ,"Black Drawer" ,2024 ));
-        service.addProduct(new Product("Mac Studio","Computer" ,"White Table" ,2025 ));
-        service.addProduct(new Product("Focusrite Mixer","Audio System" ,"White Table" ,2025 ));
-        service.addProduct(new Product("Asus Vivobook","Laptop" ,"Brown Drawer" ,2021 ));
-        service.addProduct(new Product("Asus Rog","Laptop" ,"Black Table" ,2021 ));
-        service.addProduct(new Product("Macbook Pro","Laptop" ,"Brown Drawer" ,2022 ));
-        service.addProduct(new Product("Wacom Pad","Writing Pad" ,"Black Drawer" ,2020 ));
-        service.addProduct(new Product("Apple Keyboard","Keyboard" ,"White Table" ,2022 ));
-        service.addProduct(new Product("Logitech Keyboard","Keyboard" ,"Black Table" ,2024 ));
-        service.addProduct(new Product("HDMI Cable","Cable" ,"Black Drawer" ,2024 ));
-        service.addProduct(new Product("Java Black Book","Cable" ,"Shelf" ,2024 ));
-        service.addProduct(new Product("Logitech Mouse","Mouse" ,"Black Table" ,2022 ));
-        service.addProduct(new Product("Apple Mouse","Mouse" ,"White Table" ,2022 ));
-        service.addProduct(new Product("Lenovo Mouse","Mouse" ,"Black Drawer" ,2022 ));
-        service.addProduct(new Product("BlackBeast","Computer" ,"White Table" ,2022 ));
+//        service.addProduct(new Product("Type C","Cable" ,"Black Drawer" ,2024 ));
+//        service.addProduct(new Product("Mac Studio","Computer" ,"White Table" ,2025 ));
+//        service.addProduct(new Product("Focusrite Mixer","Audio System" ,"White Table" ,2025 ));
+//        service.addProduct(new Product("Asus Vivobook","Laptop" ,"Brown Drawer" ,2021 ));
+//        service.addProduct(new Product("Asus Rog","Laptop" ,"Black Table" ,2021 ));
+//        service.addProduct(new Product("Macbook Pro","Laptop" ,"Brown Drawer" ,2022 ));
+//        service.addProduct(new Product("Wacom Pad","Writing Pad" ,"Black Drawer" ,2020 ));
+//        service.addProduct(new Product("Apple Keyboard","Keyboard" ,"White Table" ,2022 ));
+//        service.addProduct(new Product("Logitech Keyboard","Keyboard" ,"Black Table" ,2024 ));
+//        service.addProduct(new Product("HDMI Cable","Cable" ,"Black Drawer" ,2024 ));
+//        service.addProduct(new Product("Java Black Book","Cable" ,"Shelf" ,2024 ));
+//        service.addProduct(new Product("Logitech Mouse","Mouse" ,"Black Table" ,2022 ));
+//        service.addProduct(new Product("Apple Mouse","Mouse" ,"White Table" ,2022 ));
+//        service.addProduct(new Product("Lenovo Mouse","Mouse" ,"Black Drawer" ,2022 ));
+//        service.addProduct(new Product("BlackBeast","Computer" ,"White Table" ,2022 ));
 
         List<Product> products = service.getAllProducts();
-
-//        for(Product p : products) {
-//            System.out.println(p);
-//        }
+        for(Product p : products) {
+            System.out.println(p);
+        }
 
 //        Stream<Product> stm = products.stream();
 //        stm.forEach(System.out::println);
@@ -65,12 +64,12 @@ public class Main {
 //        System.out.println("###########################################################");
 //        System.out.println("a particular products out of warranty");
 //
-        List<Product> prods = service.getProductsOutOfWarranty();
+//        List<Product> prods = service.getProductsOutOfWarranty();
 //        for(Product p : prods){
 //            System.out.println(p);
 //        }
 
-        Stream<Product> stm = prods.stream();
-        stm.forEach(System.out::println);
+//        Stream<Product> stm = prods.stream();
+//        stm.forEach(System.out::println);
     }
 }
